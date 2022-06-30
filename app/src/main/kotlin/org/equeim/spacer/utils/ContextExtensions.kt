c0e8e8d0a26d6eb0c3f74c6fa069fbbaa90ceafd
Alexey Rochev <equeim@gmail.com>
2022-07-01 00:44:52 +0300

    Handle runtime locale changes

---
+++ app/src/main/kotlin/org/equeim/spacer/utils/ContextExtensions.kt
@@ -0,0 +1,20 @@
+package org.equeim.spacer.utils
+
+import android.app.Activity
+import android.app.Application
+import android.content.Context
+import android.content.ContextWrapper
+
+fun Context.getApplicationOrThrow(): Application {
+    if (this is Application) return this
+    if (this is Activity) {
+        val application = runCatching { this.application }.getOrNull()
+        if (application != null) return application
+    }
+    var applicationContext: Context? = this.applicationContext
+    while (applicationContext is ContextWrapper) {
+        if (applicationContext is Application) return applicationContext
+        applicationContext = applicationContext.baseContext
+    }
+    throw IllegalStateException("Failed to retrieve Application instance from context $this")
+}
---
+++ app/src/main/kotlin/org/equeim/spacer/utils/SystemBroadcastFlow.kt
@@ -0,0 +1,26 @@
+package org.equeim.spacer.utils
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.util.Log
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.launch
+
+private const val TAG = "SystemBroadcastFlow"
+
+fun Context.systemBroadcastFlow(intentFilter: IntentFilter): Flow<Intent> = callbackFlow {
+    val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            Log.d(TAG, "onReceive() called with: context = $context, intent = $intent")
+            launch { send(intent) }
+        }
+    }
+    registerReceiver(receiver, intentFilter)
+    awaitClose { unregisterReceiver(receiver) }
+}
+
+fun Context.systemBroadcastFlow(action: String): Flow<Intent> = systemBroadcastFlow(IntentFilter(action))
