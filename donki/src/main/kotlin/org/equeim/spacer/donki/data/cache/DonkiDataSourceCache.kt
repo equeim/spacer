06079533198d7532b11f5fe3a9b3ee62717cfcb2
Alexey Rochev <equeim@gmail.com>
2022-07-18 00:12:46 +0300

    Recreate database when cache is cleared

--- donki/src/main/kotlin/org/equeim/spacer/donki/data/cache/DonkiDataSourceCache.kt
+++ donki/src/main/kotlin/org/equeim/spacer/donki/data/cache/DonkiDataSourceCache.kt
@@ -3,46 +3,88 @@ package org.equeim.spacer.donki.data.cache
 import android.content.Context
 import android.os.storage.StorageManager
 import android.util.Log
-import androidx.annotation.VisibleForTesting
 import androidx.core.content.getSystemService
 import androidx.room.Room
+import androidx.room.RoomDatabase
 import androidx.room.withTransaction
-import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+import org.equeim.spacer.donki.CoroutineDispatchers
+import org.equeim.spacer.donki.data.Week
 import org.equeim.spacer.donki.data.cache.entities.CachedWeek
 import org.equeim.spacer.donki.data.cache.entities.toCachedEvent
 import org.equeim.spacer.donki.data.cache.entities.toExtras
 import org.equeim.spacer.donki.data.model.*
-import org.equeim.spacer.donki.data.Week
 import java.io.Closeable
+import java.nio.file.*
 import java.time.Duration
 import java.time.Instant
 
 private const val TAG = "DonkiDataSourceCache"
 private val WEEK_UP_TO_DATE_THRESHOLD: Duration = Duration.ofDays(7)
 
-private fun buildDatabase(context: Context): DonkiDatabase {
-    val databaseDirectory = context.cacheDir.toPath().resolve(DonkiDatabase.NAME)
+internal class DonkiDataSourceCache(
+    private val context: Context,
+    db: DonkiDatabase? = null,
+    private val coroutineDispatchers: CoroutineDispatchers = CoroutineDispatchers()
+) : Closeable {
+    private val coroutineScope = CoroutineScope(coroutineDispatchers.Default + SupervisorJob())
+    private var dbWatchKey: WatchKey? = null
+    private var db: DonkiDatabase
 
-    val storageManager = checkNotNull(context.getSystemService<StorageManager>())
-    storageManager.setCacheBehaviorGroup(databaseDirectory.toFile(), true)
-    storageManager.setCacheBehaviorTombstone(databaseDirectory.toFile(), true)
+    private val _databaseRecreated = MutableSharedFlow<Unit>()
+    val databaseRecreated: Flow<Unit> by ::_databaseRecreated
 
-    val databasePath = databaseDirectory.resolve(DonkiDatabase.NAME)
-    return Room.databaseBuilder(
-        context,
-        DonkiDatabase::class.java,
-        databasePath.toString()
-    ).build()
-}
+    init {
+        if (db != null) {
+            this.db = db
+        } else {
+            val databaseDirectory = context.cacheDir.toPath().resolve(DonkiDatabase.NAME)
+            this.db = createDatabase(databaseDirectory)
+            val storageManager = checkNotNull(context.getSystemService<StorageManager>())
+            coroutineScope.launch(Dispatchers.IO) {
+                Files.createDirectories(databaseDirectory)
+                storageManager.setCacheBehaviorGroup(databaseDirectory.toFile(), true)
+                storageManager.setCacheBehaviorTombstone(databaseDirectory.toFile(), true)
+                recreateDatabaseWhenDeleted(databaseDirectory)
+            }
+        }
+    }
 
-internal class DonkiDataSourceCache @VisibleForTesting constructor(private val db: DonkiDatabase) : Closeable {
-    constructor(context: Context) : this(buildDatabase(context))
+    private fun createDatabase(databaseDirectory: Path): DonkiDatabase {
+        Log.d(TAG, "createDatabase() called with: databaseDirectory = $databaseDirectory")
+        return Room.databaseBuilder(
+            context,
+            DonkiDatabase::class.java,
+            databaseDirectory.resolve(DonkiDatabase.NAME).toString()
+        ).build()
+    }
+
+    @Suppress("BlockingMethodInNonBlockingContext")
+    private suspend fun recreateDatabaseWhenDeleted(databaseDirectory: Path) {
+        val watchService = FileSystems.getDefault().newWatchService()
+        val watchKey = databaseDirectory.register(watchService, StandardWatchEventKinds.ENTRY_DELETE)
+        watchService.events(coroutineDispatchers).onEach {
+            if ((it.context() as? Path)?.toString() == DonkiDatabase.NAME) {
+                recreateDatabase(databaseDirectory)
+            }
+        }.launchIn(coroutineScope + Dispatchers.Main)
+        withContext(coroutineDispatchers.Main) {
+            dbWatchKey = watchKey
+        }
+    }
+
+    private suspend fun recreateDatabase(databaseDirectory: Path) {
+        Log.d(TAG, "recreateDatabase() called with: databaseDirectory = $databaseDirectory")
+        db.close()
+        db = createDatabase(databaseDirectory)
+        _databaseRecreated.emit(Unit)
+    }
 
     override fun close() {
         Log.d(TAG, "close() called")
+        coroutineScope.cancel()
+        dbWatchKey?.cancel()
         db.close()
     }
 
@@ -94,7 +136,11 @@ internal class DonkiDataSourceCache @VisibleForTesting constructor(private val d
             }
         } catch (e: Exception) {
             if (e !is CancellationException) {
-                Log.e(TAG, "getEventSummariesForWeek: failed to get events summaries for week = $week, eventType = $eventType", e)
+                Log.e(
+                    TAG,
+                    "getEventSummariesForWeek: failed to get events summaries for week = $week, eventType = $eventType",
+                    e
+                )
             }
             throw e
         }
@@ -151,3 +197,14 @@ internal class DonkiDataSourceCache @VisibleForTesting constructor(private val d
         GlobalScope.launch { cacheWeek(week, eventType, events, loadTime) }
     }
 }
+
+private fun WatchService.events(coroutineDispatchers: CoroutineDispatchers): Flow<WatchEvent<*>> = flow {
+    try {
+        while (currentCoroutineContext().isActive) {
+            val key = runInterruptible { take() }
+            key.pollEvents().forEach { emit(it) }
+            key.reset()
+        }
+    } catch (ignore: ClosedWatchServiceException) {
+    }
+}.flowOn(coroutineDispatchers.IO)
