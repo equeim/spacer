80ba928a9ea771cf8a8d4a4a3d85790981772324
Alexey Rochev <equeim@gmail.com>
2022-07-19 23:45:04 +0300

    Don't refresh cache if it was loaded less than hour ago

--- donki/src/test/kotlin/org/equeim/spacer/donki/data/cache/DonkiDataSourceCacheTest.kt
+++ donki/src/test/kotlin/org/equeim/spacer/donki/data/cache/DonkiDataSourceCacheTest.kt
@@ -7,23 +7,25 @@ import kotlinx.coroutines.test.runTest
 import org.equeim.spacer.donki.BaseCoroutineTest
 import org.equeim.spacer.donki.data.Week
 import org.equeim.spacer.donki.data.model.EventType
+import org.equeim.spacer.donki.data.paging.CURRENT_INSTANT
+import org.equeim.spacer.donki.timeZoneParameters
 import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
-import java.time.Duration
-import java.time.LocalDate
+import org.robolectric.ParameterizedRobolectricTestRunner
+import java.time.*
 import kotlin.test.Test
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 @OptIn(ExperimentalCoroutinesApi::class)
-@RunWith(RobolectricTestRunner::class)
-class DonkiDataSourceCacheTest : BaseCoroutineTest() {
+@RunWith(ParameterizedRobolectricTestRunner::class)
+class DonkiDataSourceCacheTest(systemTimeZone: ZoneId) : BaseCoroutineTest() {
+    private val clock = Clock.fixed(CURRENT_INSTANT, systemTimeZone)
     private val db = Room.inMemoryDatabaseBuilder(ApplicationProvider.getApplicationContext(), DonkiDatabase::class.java).build()
     private lateinit var dataSource: DonkiDataSourceCache
 
     override fun before() {
         super.before()
-        dataSource = DonkiDataSourceCache(ApplicationProvider.getApplicationContext(), db, coroutineDispatchers)
+        dataSource = DonkiDataSourceCache(ApplicationProvider.getApplicationContext(), db, coroutineDispatchers, clock)
     }
 
     override fun after() {
@@ -33,7 +35,7 @@ class DonkiDataSourceCacheTest : BaseCoroutineTest() {
 
     @Test
     fun `isWeekCachedAndNeedsRefresh() returns false when week is not cached`() = runTest {
-        assertFalse(dataSource.isWeekCachedAndNeedsRefresh(Week(LocalDate.MIN), EventType.GeomagneticStorm))
+        assertFalse(dataSource.isWeekCachedAndNeedsRefresh(Week.getCurrentWeek(clock), EventType.GeomagneticStorm))
     }
 
     @Test
@@ -46,11 +48,44 @@ class DonkiDataSourceCacheTest : BaseCoroutineTest() {
     }
 
     @Test
-    fun `isWeekCachedAndNeedsRefresh() returns false when week was loaded less than a week after last day`() = runTest {
-        val week = Week(LocalDate.MIN)
+    fun `isWeekCachedAndNeedsRefresh() returns true when week was loaded less than a week after last day and more than hour ago`() = runTest {
+        val week = Week.getCurrentWeek(clock).next()
         val eventType = EventType.GeomagneticStorm
         val loadTime = week.getInstantAfterLastDay().plus(Duration.ofDays(3))
         dataSource.cacheWeek(week, eventType, emptyList(), loadTime)
         assertTrue(dataSource.isWeekCachedAndNeedsRefresh(week, eventType))
+    }
+
+    @Test
+    fun `isWeekCachedAndNeedsRefresh() returns false when week was loaded less than a week after last day and less than hour ago`() = runTest {
+        val week = Week.getCurrentWeek(clock).next()
+        val eventType = EventType.GeomagneticStorm
+        val loadTime = Instant.now(clock).minus(Duration.ofMinutes(42))
+        dataSource.cacheWeek(week, eventType, emptyList(), loadTime)
+        assertFalse(dataSource.isWeekCachedAndNeedsRefresh(week, eventType))
+    }
+
+    @Test
+    fun `isWeekCachedAndNeedsRefresh() returns true when week was loaded before last day and more than hour ago`() = runTest {
+        val week = Week.getCurrentWeek(clock)
+        val eventType = EventType.GeomagneticStorm
+        val loadTime = Instant.now(clock).minus(Duration.ofHours(2))
+        dataSource.cacheWeek(week, eventType, emptyList(), loadTime)
+        assertTrue(dataSource.isWeekCachedAndNeedsRefresh(week, eventType))
+    }
+
+    @Test
+    fun `isWeekCachedAndNeedsRefresh() returns false when week was loaded before last day and less than hour ago`() = runTest {
+        val week = Week.getCurrentWeek(clock)
+        val eventType = EventType.GeomagneticStorm
+        val loadTime = Instant.now(clock).minus(Duration.ofMinutes(42))
+        dataSource.cacheWeek(week, eventType, emptyList(), loadTime)
+        assertFalse(dataSource.isWeekCachedAndNeedsRefresh(week, eventType))
+    }
+
+    companion object {
+        @ParameterizedRobolectricTestRunner.Parameters(name = "systemTimeZone={0}")
+        @JvmStatic
+        fun parameters(): List<ZoneId> = timeZoneParameters()
     }
 }
