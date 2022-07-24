eea988429393f845510427a97079c1104ee8f299
Alexey Rochev <equeim@gmail.com>
2022-07-25 00:38:50 +0300

    Simplify synchronization of locale-dependent state

--- app/src/main/kotlin/org/equeim/spacer/ui/screen/donki/DonkiEventsScreenViewModel.kt
+++ app/src/main/kotlin/org/equeim/spacer/ui/screen/donki/DonkiEventsScreenViewModel.kt
@@ -2,20 +2,17 @@ package org.equeim.spacer.ui.screen.donki
 
 import android.app.Application
 import android.util.Log
-import androidx.annotation.GuardedBy
 import androidx.annotation.StringRes
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import org.equeim.spacer.AppSettings
+import org.equeim.spacer.donki.data.DonkiRepository
 import org.equeim.spacer.donki.data.model.EventId
 import org.equeim.spacer.donki.data.model.EventSummary
 import org.equeim.spacer.donki.data.model.EventType
-import org.equeim.spacer.donki.data.DonkiRepository
 import org.equeim.spacer.ui.utils.defaultLocaleFlow
 import java.time.ZoneId
 import java.time.ZoneOffset
@@ -25,6 +22,7 @@ import java.time.format.DateTimeFormatterBuilder
 import java.time.format.FormatStyle
 import java.time.format.TextStyle
 import java.util.*
+import java.util.concurrent.ConcurrentHashMap
 
 private const val TAG = "DonkiEventsScreenViewModel"
 
@@ -36,13 +34,14 @@ class DonkiEventsScreenViewModel(application: Application) : AndroidViewModel(ap
     private val repository = DonkiRepository(application)
     private val settings = AppSettings(application)
 
-    private val localeDependentStateMutex = Mutex()
-    @GuardedBy("localeDependentStateMutex")
-    private lateinit var eventTimeFormatter: DateTimeFormatter
-    @GuardedBy("localeDependentStateMutex")
-    private lateinit var eventDateFormatter: DateTimeFormatter
-    @GuardedBy("localeDependentStateMutex")
-    private val eventTypesStrings = mutableMapOf<EventType, String>()
+    private class LocaleDependentState {
+        val eventTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT)
+        val eventDateFormatter: DateTimeFormatter = DateTimeFormatterBuilder().appendLocalized(FormatStyle.LONG, null).appendLiteral(' ')
+            .appendZoneText(TextStyle.SHORT).toFormatter()
+        val eventTypesStrings = ConcurrentHashMap<EventType, String>()
+    }
+    @Volatile
+    private lateinit var localeDependentState: LocaleDependentState
 
     val pagingData: Flow<PagingData<ListItem>>
 
@@ -50,7 +49,9 @@ class DonkiEventsScreenViewModel(application: Application) : AndroidViewModel(ap
         val pager = repository.getEventSummariesPager()
         val basePagingData = pager.flow.cachedIn(viewModelScope)
 
-        val defaultLocaleFlow = application.defaultLocaleFlow().onEach(::defaultLocaleChanged)
+        val defaultLocaleFlow = application.defaultLocaleFlow().onEach {
+            localeDependentState = LocaleDependentState()
+        }
         pagingData = combine(
             basePagingData,
             settings.displayEventsTimeInUTC.flow(),
@@ -77,12 +78,10 @@ class DonkiEventsScreenViewModel(application: Application) : AndroidViewModel(ap
                 when {
                     after == null -> null
                     before == null || before.zonedTime.dayOfMonth != after.zonedTime.dayOfMonth -> {
-                        localeDependentStateMutex.withLock {
-                            DateSeparator(
-                                after.eventSummary.time.epochSecond,
-                                eventDateFormatter.format(after.zonedTime)
-                            )
-                        }
+                        DateSeparator(
+                            after.eventSummary.time.epochSecond,
+                            localeDependentState.eventDateFormatter.format(after.zonedTime)
+                        )
                     }
                     else -> null
                 }
@@ -95,34 +94,21 @@ class DonkiEventsScreenViewModel(application: Application) : AndroidViewModel(ap
             }
     }
 
-    private suspend fun defaultLocaleChanged(defaultLocale: Locale) {
-        Log.d(TAG, "defaultLocaleChanged() called with: defaultLocale = $defaultLocale")
-        localeDependentStateMutex.withLock {
-            eventTimeFormatter = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT)
-            eventDateFormatter = DateTimeFormatterBuilder().appendLocalized(FormatStyle.LONG, null).appendLiteral(' ')
-                .appendZoneText(TextStyle.SHORT).toFormatter()
-            eventTypesStrings.clear()
-        }
-    }
-
     private data class EventSummaryWithZonedTime(
         val eventSummary: EventSummary,
         val zonedTime: ZonedDateTime
     )
 
-    private suspend fun EventSummaryWithZonedTime.toPresentation(): EventPresentation {
-        return localeDependentStateMutex.withLock {
-            EventPresentation(
-                id = eventSummary.id,
-                type = eventSummary.getTypeDisplayString(),
-                time = eventTimeFormatter.format(zonedTime)
-            )
-        }
+    private fun EventSummaryWithZonedTime.toPresentation(): EventPresentation {
+        return EventPresentation(
+            id = eventSummary.id,
+            type = eventSummary.getTypeDisplayString(),
+            time = localeDependentState.eventTimeFormatter.format(zonedTime)
+        )
     }
 
-    @GuardedBy("localeDependentStateMutex")
     private fun EventSummary.getTypeDisplayString(): String {
-        return eventTypesStrings.computeIfAbsent(type) { getString(type.displayStringResId) }
+        return localeDependentState.eventTypesStrings.computeIfAbsent(type) { getString(type.displayStringResId) }
     }
 
     private fun getString(@StringRes resId: Int) = getApplication<Application>().getString(resId)
