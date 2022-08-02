581d5f1af91dc4f73e6cdea65020409b0cdadf09
Alexey Rochev <equeim@gmail.com>
2022-08-03 00:39:59 +0300

    Handle dark theme changes without activity recreation and set splash screen background

--- app/src/main/kotlin/org/equeim/spacer/MainActivity.kt
+++ app/src/main/kotlin/org/equeim/spacer/MainActivity.kt
@@ -1,14 +1,16 @@
 package org.equeim.spacer
 
-import android.annotation.SuppressLint
-import android.content.Context
+import android.app.Application
 import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.MainThread
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -16,113 +18,38 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLayoutDirection
-import androidx.core.content.res.getBooleanOrThrow
-import androidx.core.content.res.use
+import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
-import androidx.lifecycle.lifecycleScope
 import dev.olshevski.navigation.reimagined.AnimatedNavHost
 import dev.olshevski.navigation.reimagined.NavBackHandler
 import dev.olshevski.navigation.reimagined.NavController
 import dev.olshevski.navigation.reimagined.rememberNavController
-import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.runBlocking
 import org.equeim.spacer.ui.screen.Destination
 import org.equeim.spacer.ui.screen.donki.DonkiEventsScreen
 import org.equeim.spacer.ui.theme.ApplicationTheme
-import org.equeim.spacer.ui.utils.isNightModeActive
-import org.equeim.spacer.ui.utils.nightMode
 import org.equeim.spacer.utils.getApplicationOrThrow
 
 private const val TAG = "MainActivity"
 
-/**
- * Singleton [AppSettings.DarkThemeMode] provider that blocks main thread when value is retrieved
- * for the first time, to not recreate [MainActivity] immediately after creation
- */
-private object DarkThemeModeProvider {
-    private val collectingScope = CoroutineScope(Dispatchers.Unconfined)
-    private var darkThemeModeModeFlow: StateFlow<AppSettings.DarkThemeMode>? = null
-
-    @MainThread
-    fun getDarkThemeMode(context: Context): AppSettings.DarkThemeMode {
-        val darkThemeModeModeFlow = this.darkThemeModeModeFlow ?: runBlocking {
-            val settings = AppSettings(context.getApplicationOrThrow())
-            settings.darkThemeMode.flow().onEach {
-                Log.d(TAG, "DarkThemeModeProvider: darkThemeMode = $it")
-            }.stateIn(collectingScope).also {
-                darkThemeModeModeFlow = it
-            }
-        }
-        return darkThemeModeModeFlow.value
-    }
-
-    fun darkThemeModeChanges(initialDarkThemeMode: AppSettings.DarkThemeMode): Flow<AppSettings.DarkThemeMode> {
-        return checkNotNull(darkThemeModeModeFlow).dropWhile { it == initialDarkThemeMode }
-    }
-}
-
 class MainActivity : ComponentActivity() {
-    private lateinit var initialDarkThemeMode: AppSettings.DarkThemeMode
-
-    override fun attachBaseContext(newBase: Context?) {
-        Log.d(TAG, "attachBaseContext() called with: newBase = $newBase")
-        super.attachBaseContext(newBase)
-        applyOverrideConfiguration(Configuration())
-    }
-
-    override fun applyOverrideConfiguration(overrideConfiguration: Configuration) {
-        Log.d(
-            TAG,
-            "applyOverrideConfiguration() called with: overrideConfiguration = $overrideConfiguration"
-        )
-        initialDarkThemeMode = DarkThemeModeProvider.getDarkThemeMode(baseContext)
-        Log.d(TAG, "applyOverrideConfiguration: initialDarkThemeMode = $initialDarkThemeMode")
-        val nightMode = initialDarkThemeMode.toNightMode(baseContext)
-        overrideConfiguration.uiMode =
-            (baseContext.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK.inv()) or nightMode
-        super.applyOverrideConfiguration(overrideConfiguration)
-    }
-
-    @SuppressLint("ResourceType")
-    @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG, "onCreate() called with: savedInstanceState = $savedInstanceState")
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "onCreate: isNightModeActive = $isNightModeActive")
-        setContent {
-            MainActivityScreen()
-        }
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        val (windowLightStatusBar, windowLightNavigationBar) = obtainStyledAttributes(
-            intArrayOf(
-                android.R.attr.windowLightStatusBar,
-                android.R.attr.windowLightNavigationBar
-            )
-        ).use {
-            it.getBooleanOrThrow(0) to it.getBooleanOrThrow(1)
-        }
-        WindowCompat.getInsetsController(window, window.decorView).apply {
-            // On some Android versions these flags may not be set correctly from theme
-            // when recreating Activity and changing night mode. Set them ourselves in code.
-            isAppearanceLightStatusBars = windowLightStatusBar
-            isAppearanceLightNavigationBars = windowLightNavigationBar
-        }
-        lifecycleScope.launch {
-            DarkThemeModeProvider.darkThemeModeChanges(initialDarkThemeMode).collect {
-                if (it.toNightMode(baseContext) != nightMode) {
-                    Log.d(TAG, "applyOverrideConfiguration: recreating activity")
-                    recreate()
-                }
-            }
+        setContent {
+            MainActivityScreen(this)
         }
     }
-}
 
-private fun AppSettings.DarkThemeMode.toNightMode(context: Context): Int {
-    return when (this) {
-        AppSettings.DarkThemeMode.FollowSystem -> context.nightMode
-        AppSettings.DarkThemeMode.On -> Configuration.UI_MODE_NIGHT_YES
-        AppSettings.DarkThemeMode.Off -> Configuration.UI_MODE_NIGHT_NO
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        Log.d(TAG, "onConfigurationChanged() called with: newConfig = $newConfig")
+        super.onConfigurationChanged(newConfig)
     }
 }
 
@@ -131,20 +58,54 @@ val LocalNavController =
 val LocalAppSettings = staticCompositionLocalOf<AppSettings> { throw IllegalStateException() }
 
 @Composable
-private fun MainActivityScreen() {
-    ApplicationTheme {
+private fun MainActivityScreen(activity: MainActivity) {
+    val context = LocalContext.current
+
+    val darkThemeMode by remember {
+        DarkThemeModeProvider.darkThemeMode(context.getApplicationOrThrow())
+    }.collectAsState()
+    LaunchedEffect(null) {
+        snapshotFlow { darkThemeMode }
+            .collect { Log.d(TAG, "darkThemeMode is $it") }
+    }
+
+    val isSystemInDarkTheme by rememberUpdatedState(isSystemInDarkTheme())
+    LaunchedEffect(null) {
+        snapshotFlow { isSystemInDarkTheme }
+            .collect { Log.d(TAG, "isSystemInDarkTheme is $it") }
+    }
+
+    val isDarkTheme by remember {
+        derivedStateOf {
+            when (darkThemeMode) {
+                AppSettings.DarkThemeMode.FollowSystem -> isSystemInDarkTheme
+                AppSettings.DarkThemeMode.On -> true
+                AppSettings.DarkThemeMode.Off -> false
+            }
+        }
+    }
+    val window = activity.window
+    val view = LocalView.current
+    LaunchedEffect(window, view) {
+        snapshotFlow { isDarkTheme }
+            .onEach { Log.d(TAG, "isDarkTheme is $it") }
+            .collect { setDarkThemeWindowProperties(window, view, it) }
+    }
+
+    ApplicationTheme(isDarkTheme) {
         val insetsPadding = WindowInsets.systemBars.asPaddingValues()
         val layoutDirection = LocalLayoutDirection.current
         Surface(
-            modifier = Modifier.fillMaxSize().padding(
-                start = insetsPadding.calculateStartPadding(layoutDirection),
-                end = insetsPadding.calculateEndPadding(layoutDirection)
-            ),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(
+                    start = insetsPadding.calculateStartPadding(layoutDirection),
+                    end = insetsPadding.calculateEndPadding(layoutDirection)
+                ),
             color = MaterialTheme.colors.background
         ) {
             val navController = rememberNavController<Destination>(DonkiEventsScreen)
-            val context = LocalContext.current
-            val settings = remember(context) { AppSettings(context.getApplicationOrThrow()) }
+            val settings = remember { AppSettings(context.getApplicationOrThrow()) }
             CompositionLocalProvider(
                 LocalNavController provides navController,
                 LocalAppSettings provides settings
@@ -154,5 +115,36 @@ private fun MainActivityScreen() {
                 AnimatedNavHost(navController) { it.Content() }
             }
         }
+    }
+}
+
+/**
+ * Singleton [AppSettings.DarkThemeMode] provider that blocks current thread when value is retrieved
+ * for the first time, to not recompose everything immediately after creation
+ */
+private object DarkThemeModeProvider {
+    private val collectingScope = CoroutineScope(Dispatchers.Unconfined)
+    @Volatile
+    private var darkThemeModeModeFlow: StateFlow<AppSettings.DarkThemeMode>? = null
+
+    fun darkThemeMode(application: Application): StateFlow<AppSettings.DarkThemeMode> {
+        return this.darkThemeModeModeFlow ?: runBlocking {
+            val settings = AppSettings(application)
+            settings.darkThemeMode.flow().stateIn(collectingScope).also {
+                darkThemeModeModeFlow = it
+            }
+        }
+    }
+}
+
+private fun setDarkThemeWindowProperties(window: Window, view: View, isDarkTheme: Boolean) {
+    WindowCompat.getInsetsController(window, view).apply {
+        isAppearanceLightStatusBars = !isDarkTheme
+        isAppearanceLightNavigationBars = !isDarkTheme
+    }
+    if (Build.VERSION.SDK_INT < 29) {
+        window.navigationBarColor = view.context.getColor(
+            if (isDarkTheme) R.color.navigation_bar_color_dark_theme else R.color.navigation_bar_color_light_theme
+        )
     }
 }
