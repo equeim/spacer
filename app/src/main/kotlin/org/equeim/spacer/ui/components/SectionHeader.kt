5cbdb97f24eda5ae14b44ec6905b7cf5ba697c68
Alexey Rochev <equeim@gmail.com>
2022-08-06 01:05:07 +0300

    Improve settigns screen padding

--- app/src/main/kotlin/org/equeim/spacer/ui/components/SectionHeader.kt
+++ app/src/main/kotlin/org/equeim/spacer/ui/components/SectionHeader.kt
@@ -9,24 +9,37 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.Dp
 import org.equeim.spacer.ui.theme.Dimens
 
 @Composable
-fun SectionHeader(title: String, modifier: Modifier = Modifier, style: TextStyle = MaterialTheme.typography.h6) {
+fun SectionHeader(
+    title: String,
+    modifier: Modifier = Modifier,
+    topPadding: Dp = Dimens.SpacingMedium,
+    bottomPadding: Dp = Dimens.SpacingMedium - Dimens.SpacingSmall,
+    style: TextStyle = MaterialTheme.typography.h6
+) {
     Text(
         title,
-        modifier.padding(top = Dimens.SpacingMedium, bottom = Dimens.SpacingMedium - Dimens.SpacingSmall),
+        modifier.padding(top = topPadding, bottom = bottomPadding),
         color = MaterialTheme.colors.primary,
         style = style
     )
 }
 
 @Composable
-fun SectionPlaceholder(title: String, modifier: Modifier = Modifier, style: TextStyle = MaterialTheme.typography.h6) {
+fun SectionPlaceholder(
+    title: String,
+    modifier: Modifier = Modifier,
+    topPadding: Dp = Dimens.SpacingMedium,
+    bottomPadding: Dp = Dimens.SpacingMedium - Dimens.SpacingSmall,
+    style: TextStyle = MaterialTheme.typography.h6
+) {
     CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
         Text(
             title,
-            modifier.padding(top = Dimens.SpacingMedium, bottom = Dimens.SpacingMedium - Dimens.SpacingSmall),
+            modifier.padding(top = topPadding, bottom = bottomPadding),
             style = style
         )
     }
