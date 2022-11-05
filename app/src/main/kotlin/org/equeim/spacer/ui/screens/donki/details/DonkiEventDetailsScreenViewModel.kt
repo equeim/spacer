772fa5f4b1550e567a76b486c5e569dda6a573c1
Alexey Rochev <equeim@gmail.com>
2022-11-05 20:58:40 +0300

    DonkiEventDetailsScreen: show refresh indicator with delay

--- app/src/main/kotlin/org/equeim/spacer/ui/screens/donki/details/DonkiEventDetailsScreenViewModel.kt
+++ app/src/main/kotlin/org/equeim/spacer/ui/screens/donki/details/DonkiEventDetailsScreenViewModel.kt
@@ -7,20 +7,18 @@ package org.equeim.spacer.ui.screens.donki.details
 import android.app.Application
 import android.util.Log
 import androidx.annotation.StringRes
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 import org.equeim.spacer.AppSettings
-import org.equeim.spacer.ui.LocalDefaultLocale
 import org.equeim.spacer.donki.data.DonkiRepository
 import org.equeim.spacer.donki.data.model.Event
 import org.equeim.spacer.donki.data.model.EventId
 import org.equeim.spacer.donki.data.model.EventType
+import org.equeim.spacer.ui.LocalDefaultLocale
 import org.equeim.spacer.ui.screens.donki.displayStringResId
 import org.equeim.spacer.ui.utils.defaultLocaleFlow
 import java.time.Instant
@@ -29,18 +27,51 @@ import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 import java.util.concurrent.ConcurrentHashMap
+import kotlin.time.Duration.Companion.milliseconds
 
 private const val TAG = "DonkiEventDetailsScreenViewModel"
+private val REFRESH_INDICATOR_DELAY = 300.milliseconds
 
 class DonkiEventDetailsScreenViewModel(private val eventId: EventId, application: Application) :
     AndroidViewModel(application) {
 
     private val repository = DonkiRepository(application)
 
-    var contentUiState: ContentUiState by mutableStateOf(ContentUiState.Loading)
-        private set
-    var refreshing: Boolean by mutableStateOf(false)
-        private set
+    private val _contentUiState = MutableStateFlow<ContentUiState>(ContentUiState.Loading)
+    val contentUiState: StateFlow<ContentUiState> by ::_contentUiState
+
+    private enum class RefreshingState {
+        NotRefreshing,
+        RefreshingAutomatically,
+        RefreshingManually
+    }
+    private val refreshing = MutableStateFlow(RefreshingState.NotRefreshing)
+
+    private enum class RefreshIndicatorState {
+        Show,
+        ShowDelayed,
+        Hide
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val showRefreshIndicator: StateFlow<Boolean> =
+        combine(contentUiState, refreshing) { contentUiState, refreshing ->
+            when {
+                refreshing == RefreshingState.RefreshingManually -> RefreshIndicatorState.Show
+                contentUiState is ContentUiState.Loading
+                        || refreshing == RefreshingState.RefreshingAutomatically -> RefreshIndicatorState.ShowDelayed
+                else -> RefreshIndicatorState.Hide
+            }
+        }
+            .onEach { Log.d(TAG, "Refresh indicator state: $it") }
+            .distinctUntilChanged()
+            .mapLatest {
+                if (it == RefreshIndicatorState.ShowDelayed) delay(REFRESH_INDICATOR_DELAY)
+                it != RefreshIndicatorState.Hide
+            }
+            .distinctUntilChanged()
+            .onEach { Log.d(TAG, "Show refresh indicator: $it") }
+            .stateIn(viewModelScope, SharingStarted.Eagerly, false)
 
     private class LocaleDependentState {
         val eventTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG)
@@ -71,23 +102,22 @@ class DonkiEventDetailsScreenViewModel(private val eventId: EventId, application
     }
 
     private suspend fun load(forceRefresh: Boolean) {
-        if (contentUiState is ContentUiState.Error) {
-            contentUiState = ContentUiState.Loading
-        }
+        _contentUiState.compareAndSet(ContentUiState.Error, ContentUiState.Loading)
         val eventById = repository.getEventById(eventId, forceRefresh = forceRefresh)
+        val newUiState = withContext(Dispatchers.Default) { eventById.toContentUiState() }
         if (!forceRefresh && eventById.needsRefreshing) {
-            refresh()
+            refresh(fromUi = false)
         }
-        contentUiState = withContext(Dispatchers.Default) { eventById.toContentUiState() }
+        _contentUiState.value = newUiState
     }
 
-    fun refresh() {
+    fun refresh(fromUi: Boolean = true) {
+        refreshing.value = if (fromUi) RefreshingState.RefreshingManually else RefreshingState.RefreshingAutomatically
         viewModelScope.launch {
-            refreshing = true
             try {
                 load(forceRefresh = true)
             } finally {
-                refreshing = false
+                refreshing.value = RefreshingState.NotRefreshing
             }
         }
     }
