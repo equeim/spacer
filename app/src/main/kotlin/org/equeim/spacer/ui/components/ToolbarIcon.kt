53f21ed26526e98ef7aa6f884d38eb0ee15ac5ec
Alexey Rochev <equeim@gmail.com>
2023-03-27 00:25:24 +0300

    Add tooltips for toolbar icons

---
+++ app/src/main/kotlin/org/equeim/spacer/ui/components/ToolbarIcon.kt
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: 2022 Alexey Rochev
+//
+// SPDX-License-Identifier: MIT
+
+package org.equeim.spacer.ui.components
+
+import androidx.annotation.StringRes
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ToolbarIcon(icon: ImageVector, @StringRes textId: Int, onClick: () -> Unit) {
+    val text = stringResource(textId)
+    PlainTooltipBox(tooltip = { Text(text) }) {
+        IconButton(onClick, Modifier.tooltipAnchor()) {
+            Icon(icon, text)
+        }
+    }
+}
