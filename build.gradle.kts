203c1e4f07a436546bf6c7535a8d62e1281b94f7
Alexey Rochev <equeim@gmail.com>
2023-12-02 01:28:04 +0300

    Fix deprecation warning

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 }
 
 tasks.register<Delete>("clean") {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)
 }
 
 tasks.named<DependencyUpdatesTask>("dependencyUpdates") {
