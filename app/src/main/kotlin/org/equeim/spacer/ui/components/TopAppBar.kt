941c5145ae06450fc904c0dcbc0dc8779d804a5c
Alexey Rochev <equeim@gmail.com>
2024-01-30 22:44:04 +0300

    Update dependencies

--- app/src/main/kotlin/org/equeim/spacer/ui/components/TopAppBar.kt
+++ app/src/main/kotlin/org/equeim/spacer/ui/components/TopAppBar.kt
@@ -4,10 +4,19 @@
 
 package org.equeim.spacer.ui.components
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.*
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -52,7 +61,7 @@ fun SubScreenTopAppBar(
         modifier = modifier,
         navigationIcon = {
             val navController = LocalNavController.current
-            ToolbarIcon(Icons.Filled.ArrowBack, R.string.navigate_up, navController::pop)
+            ToolbarIcon(Icons.AutoMirrored.Filled.ArrowBack, R.string.navigate_up, navController::pop)
         },
         actions = actions,
         colors = TopAppBarDefaults.topAppBarColors(
