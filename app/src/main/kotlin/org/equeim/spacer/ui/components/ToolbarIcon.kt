f39aac478f9048b00c76cdb653cadd3a4535cc06
Alexey Rochev <equeim@gmail.com>
2024-01-30 23:24:08 +0300

    Update material3 library to 1.2.0-rc01

--- app/src/main/kotlin/org/equeim/spacer/ui/components/ToolbarIcon.kt
+++ app/src/main/kotlin/org/equeim/spacer/ui/components/ToolbarIcon.kt
@@ -5,9 +5,15 @@
 package org.equeim.spacer.ui.components
 
 import androidx.annotation.StringRes
-import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.PlainTooltip
+import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 
@@ -15,8 +21,12 @@ import androidx.compose.ui.res.stringResource
 @Composable
 fun ToolbarIcon(icon: ImageVector, @StringRes textId: Int, onClick: () -> Unit) {
     val text = stringResource(textId)
-    PlainTooltipBox(tooltip = { Text(text) }) {
-        IconButton(onClick, Modifier.tooltipAnchor()) {
+    TooltipBox(positionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(), tooltip = {
+        PlainTooltip {
+            Text(text)
+        }
+    }, state = rememberTooltipState()) {
+        IconButton(onClick) {
             Icon(icon, text)
         }
     }
