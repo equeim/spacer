f03d1da0df96f6aa4a04a12721e7da10e52e3f9d
Alexey Rochev <equeim@gmail.com>
2024-11-10 02:27:55 +0300

    Improve dependencyUpdates task error handling

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,12 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import java.util.Locale
 
 plugins {
-    alias(libs.plugins.android.application) apply(false)
-    alias(libs.plugins.android.library) apply(false)
-    alias(libs.plugins.kotlin.android) apply(false)
-    alias(libs.plugins.kotlin.plugin.parcelize) apply(false)
-    alias(libs.plugins.kotlin.plugin.serialization) apply(false)
-    alias(libs.plugins.ksp) apply(false)
+    alias(libs.plugins.android.application) apply (false)
+    alias(libs.plugins.android.library) apply (false)
+    alias(libs.plugins.kotlin.android) apply (false)
+    alias(libs.plugins.kotlin.plugin.parcelize) apply (false)
+    alias(libs.plugins.kotlin.plugin.serialization) apply (false)
+    alias(libs.plugins.ksp) apply (false)
     alias(libs.plugins.gradle.versions)
 }
 
@@ -23,8 +23,11 @@ tasks.named<DependencyUpdatesTask>("dependencyUpdates") {
     gradleReleaseChannel = "current"
     val channelProvider = VersionChannelProvider()
     rejectVersionIf {
-        val currentChannel = channelProvider.getChannel(currentVersion)
-        val candidateChannel = channelProvider.getChannel(candidate.version)
+        val currentChannel = channelProvider.getChannel(candidate.moduleIdentifier, currentVersion)
+            ?: return@rejectVersionIf false
+        val candidateChannel =
+            channelProvider.getChannel(candidate.moduleIdentifier, candidate.version)
+                ?: return@rejectVersionIf false
         candidateChannel < currentChannel
     }
 }
@@ -38,18 +41,21 @@ class VersionChannelProvider {
 
         constructor(vararg keywords: String) : this(keywords.asList())
 
-        fun matches(versionLowercase: String): Boolean = keywords.any { versionLowercase.contains(it) }
+        fun matches(versionLowercase: String): Boolean =
+            keywords.any { versionLowercase.contains(it) }
     }
-    private val channels = Channel.values()
-    private val stableVersionRegex = "^[0-9.]+$".toRegex()
 
-    fun getChannel(version: String): Channel {
+    private val channels = Channel.values()
+    private val stableVersionRegex = "^[0-9.-]+$".toRegex()
+
+    fun getChannel(dependency: ModuleIdentifier, version: String): Channel? {
         val versionLowercase = version.lowercase(Locale.ROOT)
         if (versionLowercase.matches(stableVersionRegex)) {
             return Channel.Stable
         }
         val channelFromKeyword = channels.find { it.matches(versionLowercase) }
         if (channelFromKeyword != null) return channelFromKeyword
-        throw RuntimeException("Failed to determine channel for version '$version'")
+        logger.error("Failed to determine channel for dependency $dependency with version '$version'")
+        return null
     }
 }
