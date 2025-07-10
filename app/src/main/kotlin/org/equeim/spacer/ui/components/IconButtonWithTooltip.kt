20bd538e1277da4f2a6ae496b7eaa454a0fb866d
Alexey Rochev <equeim@gmail.com>
2025-07-10 04:38:03 +0300

    Show filters button as FAB

--- app/src/main/kotlin/org/equeim/spacer/ui/components/IconButtonWithTooltip.kt
+++ app/src/main/kotlin/org/equeim/spacer/ui/components/IconButtonWithTooltip.kt
@@ -4,7 +4,6 @@
 
 package org.equeim.spacer.ui.components
 
-import android.view.HapticFeedbackConstants
 import androidx.annotation.StringRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -19,21 +18,14 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonColors
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.LocalContentColor
-import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.Text
-import androidx.compose.material3.TooltipBox
-import androidx.compose.material3.TooltipDefaults
 import androidx.compose.material3.minimumInteractiveComponentSize
-import androidx.compose.material3.rememberTooltipState
 import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.LocalView
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
 
@@ -63,31 +55,6 @@ fun IconButtonWithTooltipAndBadge(
             ) {
                 Icon(icon, text)
             }
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun BaseButtonWithTooltip(
-    @StringRes tooltipText: Int,
-    modifier: Modifier = Modifier,
-    button: @Composable (tooltipText: String) -> Unit
-) {
-    val tooltipTextString = stringResource(tooltipText)
-    // Passing modifier to TooltipBox is broken
-    Box(modifier) {
-        TooltipBox(
-            positionProvider = TooltipDefaults.rememberTooltipPositionProvider(),
-            tooltip = {
-                PlainTooltip { Text(tooltipTextString) }
-                val view = LocalView.current
-                LaunchedEffect(null) { view.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS) }
-            },
-            state = rememberTooltipState(),
-            focusable = false
-        ) {
-            button(tooltipTextString)
         }
     }
 }
