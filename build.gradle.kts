fe02d35927f196f12cc75bc487d8bdbd3a05ed23
Alexey Rochev <equeim@gmail.com>
2026-02-08 04:41:19 +0300

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,10 @@
 plugins {
     alias(libs.plugins.android.application) apply (false)
     alias(libs.plugins.android.library) apply (false)
-    alias(libs.plugins.kotlin.android) apply (false)
+    alias(libs.plugins.kotlin) apply (false)
     alias(libs.plugins.kotlin.plugin.parcelize) apply (false)
     alias(libs.plugins.kotlin.plugin.serialization) apply (false)
+    alias(libs.plugins.kotlin.plugin.compose) apply (false)
     alias(libs.plugins.ksp) apply (false)
     alias(libs.plugins.deezer.caupain)
 }
--- retrofit-provider/build.gradle.kts
+++ retrofit-provider/build.gradle.kts
@@ -6,7 +6,6 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
 }
 
 android {
